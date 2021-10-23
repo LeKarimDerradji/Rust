0c596119bb0808ae341abe472bf7171c3100fccb
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-10-24 00:07:38 +0200

    err_immutable

---
+++ variables/target/debug/deps/variables-27f88e652e07e839.d
@@ -0,0 +1,5 @@
+/home/squad/programming/Rust/variables/target/debug/deps/variables-27f88e652e07e839.rmeta: src/main.rs
+
+/home/squad/programming/Rust/variables/target/debug/deps/variables-27f88e652e07e839.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-8acf8f5e2e21b6c0.d
@@ -0,0 +1,5 @@
+/home/squad/programming/Rust/variables/target/debug/deps/variables-8acf8f5e2e21b6c0.rmeta: src/main.rs
+
+/home/squad/programming/Rust/variables/target/debug/deps/variables-8acf8f5e2e21b6c0.d: src/main.rs
+
+src/main.rs:
