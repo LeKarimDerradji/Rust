46b9f93124337c703bb68285307385e8a3a08093
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-10-23 21:18:42 +0200

    looping_and_break

--- rust_guessing_game/src/main.rs
+++ rust_guessing_game/src/main.rs
@@ -1,30 +1,35 @@
 // Importing libraries with particular traits
-use std::io::{self};
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io::{self};
 fn main() {
     println!("Guess the number!");
-    // Consuming the rand library by declaring a variable that is set to a random number between 1 & 0 
-    // The random number is within the thread of the OS. Gen_Range is to control the range. 
+    // Consuming the rand library by declaring a variable that is set to a random number between 1 & 0
+    // The random number is within the thread of the OS. Gen_Range is to control the range.
     let secret_number = rand::thread_rng().gen_range(1..111);
-    // Printing that number to test that rand works. 
+    // Printing that number to test that rand works.
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess: ");
+    loop {
+        println!("Please input your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
 
-    println!("You guessed {}", guess);
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Guessed The Secret Number!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Guessed The Secret Number!");
+                break;
+            }
+        }
     }
 }
