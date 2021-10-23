9017608d934fe692802de78e4004f732283ee74b
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-10-24 00:10:36 +0200

    adding_mut_keyword

---
+++ variables/target/debug/deps/variables-dbbc8040ef63580b.d
@@ -0,0 +1,5 @@
+/home/squad/programming/Rust/variables/target/debug/deps/variables-dbbc8040ef63580b: src/main.rs
+
+/home/squad/programming/Rust/variables/target/debug/deps/variables-dbbc8040ef63580b.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/squad/programming/Rust/variables/target/debug/variables: /home/squad/programming/Rust/variables/src/main.rs
