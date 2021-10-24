d5258297961b532ad305de7c1251a2f7c0d7deee
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-10-24 15:59:20 +0200

    shadowing_scope

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,16 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    println!("Shadowing");
+
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y is {}", y);
+    }
+
+    println!("The value of y is {}", y);
 }
